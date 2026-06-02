bd2f5a0dfa3912b44a9be468e96999c1a53c63df
Chrissmanuel <christian.caro@correomc.com>
2026-06-02 16:51:00 -0400

    quitar actualización de ids y logs

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def enviar_a_google_sheets(http: HttpClient, datos: List[Dict[str, Any]], sheet:
 
 def main() -> None:
     configurar_logging()
-    migrar_ids_monnet()    # <--- EJECUTAR UNA SOLA VEZ
+    #migrar_ids_monnet()    # <--- EJECUTAR UNA SOLA VEZ
 
     http = HttpClient()
 
